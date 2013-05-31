8c29f059701bdc836a437c575c902d1617458543
Barry Wark <barrywark@gmail.com>
2013-05-31 09:59:28 -0400

    Use ovation.importer.import_main

--- field_data/__main__.py
+++ field_data/__main__.py
@@ -1,77 +1,54 @@
 #Copyright (c) 2013, Physion Consulting
 
 import sys
-import argparse
-import getpass
-import logging
 
 from field_data.importer import import_csv, CSV_HEADER_ROW, FIRST_MEASUREMENT_COLUMN_NUMBER, DATE_COLUMN_NUMBER
-from ovation.connection import connect
+from ovation.importer import import_main
 
 DESCRIPTION = """Import field CSV data into the local Ovation database.
 """
 
 def main(args=sys.argv, dsc=None):
 
-    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    def parser_callback(parser):
+        csv_group = parser.add_argument_group("CSV")
+        csv_group.add_argument('--header-rows',
+                               help='number of csv header rows. Default = 2',
+                               default=CSV_HEADER_ROW)
+        csv_group.add_argument('--date-column',
+                              help='column number of measurement date (0-indexed). Default = {}'.format(DATE_COLUMN_NUMBER),
+                              default=DATE_COLUMN_NUMBER)
+        csv_group.add_argument('--measurement-column',
+                               help='column number of first measurement. Default = {}'.format(FIRST_MEASUREMENT_COLUMN_NUMBER),
+                               default=FIRST_MEASUREMENT_COLUMN_NUMBER)
 
-    auth_group = parser.add_argument_group('authentication')
-    auth_group.add_argument('--user', help='ovation.io user email')
-    auth_group.add_argument('--password', help='ovation.io password')
-
-    parser.add_argument('files', metavar='f.csv', nargs='+', type=str)#argparse.FileType('r'))
-
-    csv_group = parser.add_argument_group("CSV")
-    csv_group.add_argument('--header-rows',
-                           help='number of csv header rows. Default = 2',
-                           default=CSV_HEADER_ROW)
-    csv_group.add_argument('--date-column',
-                          help='column number of measurement date (0-indexed). Default = {}'.format(DATE_COLUMN_NUMBER),
-                          default=DATE_COLUMN_NUMBER)
-    csv_group.add_argument('--measurement-column',
-                           help='column number of first measurement. Default = {}'.format(FIRST_MEASUREMENT_COLUMN_NUMBER),
-                           default=FIRST_MEASUREMENT_COLUMN_NUMBER)
-
-    parser.add_argument('--timezone',
-                        default=None,
-                        help='timezone name in which data was collected. Default = local time zone')
-
-    experiment_group = parser.add_argument_group('epoch container')
-    experiment_group.add_argument('--container', help='epoch container ID', required=True)
-    experiment_group.add_argument('--protocol', help='protocol ID', required=True)
+        return parser
 
 
-    args = parser.parse_args(args=args)
+    def import_wrapper(ctx,
+                       container_id=None,
+                       protocol_id=None,
+                       files=None,
+                       timezone=None,
+                       **args):
 
-    logging.basicConfig(filename='import.log',level=logging.INFO)
-
-
-
-    if dsc is None:
-        if args.user is None:
-            args.user = raw_input('ovation.io user: ')
-
-        if args.password is None:
-            password = getpass.getpass(prompt='ovation.io password: ')
-        else:
-            password = args.password
-
-        dsc = connect(args.user, password)
-
-    ctx = dsc.getContext()
-
-    try:
         return import_csv(ctx,
-                          container_id=args.container,
-                          protocol_id=args.protocol,
-                          files=args.files,
-                          timezone=args.timezone,
+                          container_id=container_id,
+                          protocol_id=protocol_id,
+                          files=files,
+                          timezone=timezone,
                           csv_header_row=args.header_rows,
                           date_column=args.date_column,
                           first_measurement_column=args.measurement_column)
-    except Exception:
-        logging.error('Unable to import {}'.format(args.files), exc_info=True)
-        return 1
+
+    return import_main(args=args,
+                       name='field_data',
+                       description=DESCRIPTION,
+                       file_ext='csv',
+                       parser_callback=parser_callback,
+                       import_fn=import_wrapper,
+                       dsc=dsc)
+
 
 
 if __name__ == '__main__':
